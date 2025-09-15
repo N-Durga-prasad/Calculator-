d00c6e30544b0100753dbcd3303cab290d293593
Nagaram Durga Prasad <nagaramdurgaprasad8500@gmail.com>
2025-09-15 11:52:07 +0530

    Add files via upload

---
+++ dp.py
@@ -0,0 +1,14 @@
+a=int(input("enter number: "))
+print("select operator= +,-,*,/")
+oper=input("pic one operator  :")
+b=int(input("enter number: "))
+if oper=='+':
+      print(a+b)
+elif oper=='-':
+      print(a-b)
+elif oper=='*':
+      print(a*b)
+elif oper=='/':
+      print(a/b)
+else:
+    print("invalid operator")
