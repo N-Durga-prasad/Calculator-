e92c050944cbe5b51435b41fb08c467296df7b7c
Nagaram Durga Prasad <nagaramdurgaprasad8500@gmail.com>
2025-09-24 10:58:23 +0530

    Update dp.py

--- dp.py
+++ dp.py
@@ -1,18 +1,21 @@
 import os
 
-a = int(os.environ.get("NUMBER", 0)) 
-print("Number entered:", a)
-print("select operator= +,-,*,/")
-oper=input("pic one operator  :")
-b=int(input("enter number: "))
-if oper=='+':
-      print(a+b)
-elif oper=='-':
-      print(a-b)
-elif oper=='*':
-      print(a*b)
-elif oper=='/':
-      print(a/b)
-else:
-    print("invalid operator")
+# Get values from environment variables
+a = int(os.getenv('NUM1', '0'))    # Default to 0 if not set
+b = int(os.getenv('NUM2', '0'))
+oper = os.getenv('OP', '+')        # Default operator +
 
+print(f"Number 1: {a}")
+print(f"Number 2: {b}")
+print(f"Operator: {oper}")
+
+if oper == '+':
+    print("Result:", a + b)
+elif oper == '-':
+    print("Result:", a - b)
+elif oper == '*':
+    print("Result:", a * b)
+elif oper == '/':
+    print("Result:", a / b)
+else:
+    print("Invalid operator")
