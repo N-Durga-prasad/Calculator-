527b4b052e2d84322b611f28fb2f4d42e5653268
Nagaram Durga Prasad <nagaramdurgaprasad8500@gmail.com>
2025-09-24 10:50:15 +0530

    Update dp.py

--- dp.py
+++ dp.py
@@ -1,4 +1,7 @@
-a=int(input("enter number: "))
+import os
+
+a = int(os.environ.get("NUMBER", 0)) 
+print("Number entered:", a)
 print("select operator= +,-,*,/")
 oper=input("pic one operator  :")
 b=int(input("enter number: "))
@@ -12,3 +15,4 @@ elif oper=='/':
       print(a/b)
 else:
     print("invalid operator")
+
